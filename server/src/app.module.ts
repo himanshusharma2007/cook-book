376b9b977d4863aa5144b83165f868db246a62ef
Himanshu sharma <himanshudeveloper0@gmail.com>
2025-07-02 13:22:02 +0530

    auth middleware added

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,22 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { DatabaseConfig } from "./config/database.config";
+import { AuthModule } from "./routes/auth.module";
+import { RecipesModule } from "./routes/recipes.module";
+import { FavoritesModule } from "./routes/favorites.module";
+import { User } from "./models/user.model";
+import { Recipe } from "./models/recipe.model";
+import { Favorite } from "./models/favorite.model";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRootAsync({
+      useClass: DatabaseConfig,
+    }),
+    SequelizeModule.forFeature([User, Recipe, Favorite]),
+    AuthModule,
+    RecipesModule,
+    FavoritesModule,
+  ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,16 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import * as cookieParser from "cookie-parser";
+import { initializeDatabase } from "./models/index";
+import { join } from "path";
+import { NestExpressApplication } from "@nestjs/platform-express";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(cookieParser());
+  app.useStaticAssets(join(__dirname, "..", "uploads"), { prefix: "/uploads/" });
+
+  await initializeDatabase();
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
